9880cdd354e0d985eaeb847abca283399fcb3804
Damian <damianordonezp@gmail.com>
2024-08-09 00:16:15 +0200

    start

---
+++ src/main/java/com/dharian/rest/webservices/restful_web_services/user/UserDaoService.java
@@ -0,0 +1,34 @@
+package com.dharian.rest.webservices.restful_web_services.user;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UserDaoService {
+
+    private static List<User> users = new ArrayList<>();
+
+    static {
+        users.add(new User(1, "Paco", LocalDate.now().minusYears(30)));
+        users.add(new User(2, "Maria", LocalDate.now().minusYears(30)));
+        users.add(new User(3, "Jose", LocalDate.now().minusYears(30)));
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    public User findById(int id) {
+
+        return users.stream()
+                .filter(user -> user.getId() == id)
+                .findFirst().get();
+    }
+
+    public User save(User user) {
+        return null;
+    }
+}
